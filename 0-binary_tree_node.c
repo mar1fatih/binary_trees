97c6fa8d52309b3640d9e3ab4c3eb026a2082dfc
mar1fatih <marouanefatih631@gmail.com>
2024-01-02 21:44:58 +0000

    task 0

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,28 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_node - create new tree element
+ * @parent: parent
+ * @value: element value
+ * Return: binary_tree_t
+*/
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	binary_tree_t *newnode;
+
+	newnode = malloc(sizeof(binary_tree_t));
+	if (!newnode)
+		return (NULL);
+	if (!parent)
+	{
+		newnode->n = value;
+		newnode->parent = NULL;
+		newnode->left = NULL;
+		newnode->right = NULL;
+		return (newnode);
+	}
+	newnode->n = value;
+	newnode->parent = parent;
+	newnode->left = NULL;
+	newnode->right = NULL;
+	return (newnode);
+}
