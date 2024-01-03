1f05571f913d236e63eb9a5bd080de3b84e611b7
mar1fatih <marouanefatih631@gmail.com>
2024-01-03 16:34:28 +0000

    task 7

---
+++ 7-binary_tree_inorder.c
@@ -0,0 +1,14 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_inorder - printing a tree inorder
+ * @tree: tree
+ * @func: func
+*/
+void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (tree == NULL || func == NULL)
+		return;
+	binary_tree_inorder(tree->left, func);
+	func(tree->n);
+	binary_tree_inorder(tree->right, func);
+}
