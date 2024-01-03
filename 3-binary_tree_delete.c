cf17a22420c16a58a1b45ddfc4ae8489fe9b7154
mar1fatih <marouanefatih631@gmail.com>
2024-01-03 14:33:49 +0000

    task 3

---
+++ 3-binary_tree_delete.c
@@ -0,0 +1,13 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_delete - free all nodes
+ * @tree: root
+*/
+void binary_tree_delete(binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return;
+	binary_tree_delete(tree->left);
+	binary_tree_delete(tree->right);
+	free(tree);
+}
